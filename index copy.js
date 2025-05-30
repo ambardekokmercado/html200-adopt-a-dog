47f03dc2d81d807f800a8b33ef8843a7d99307a8
adekokmercado <131508655+ambardekokmercado@users.noreply.github.com>
2025-05-29 20:51:06 -0700

    lesson 5-challenge 2
    Dog info. and cost calculator (accumulation)

---
+++ index copy.js
@@ -0,0 +1,33 @@
+//Write a simple webpage that contains a button. 
+ //When users click the button it should open a prompt that takes a number 
+ //from the user and squares it. Next, it should open a second alert with the result. 
+ //Create an HTML page and write your JavaScript in a script tag, or create a JavaScript 
+ //file and link to it from the HTML page.
+
+//You must use a function and an event handler.
+
+
+//Declare variables (?)
+//Name function
+//Result 
+
+
+
+function dogInfo(name,breed,price) { 
+alert ('Name: '+name+' Breed: '+breed+' Price:'+price);
+//alert (`Name: ${name}`);
+} 
+
+
+let cumulativePrice = 0;
+function addTotal(price){
+cumulativePrice = cumulativePrice+price;
+alert (cumulativePrice);
+}
+
+
+
+//function numberSquared (firstNumber,firstNumber)
+//result = firstNumber*firstNumber
+
+//numberSquared (x,x)
